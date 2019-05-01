29a16ca22fd5a6f32fc2e5a821efc160210cf656
Ferdinand <flitz.tugano@gmail.com>
2019-05-02 07:43:25 +0800

    Redesign header, added some cool tricks

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,84 +3,121 @@
 // An object literal
 var app = {
   init: function() {
-    app.functionOne();
+    app.burgerMenu();
+    app.typingHeadline();
+    app.isotope();
+    app.aos();
   },
-  functionOne: function() {}
+  // ****** ****** ****** ******
+  // START Burger Menu
+  // ****** ****** ****** ******
+  burgerMenu: function() {
+    // Select DOM Items
+    var menuBtn = document.querySelector(".menu-btn");
+    var menu = document.querySelector(".menu");
+    var menuUl = document.querySelector(".menu-ul");
+    var overlay = document.querySelector(".overlay");
+
+    // var navItems = document.querySelectorAll(".nav-item");
+    // var body = document.querySelector("body");
+
+    // Set Initial State Of Menu
+    var showMenu = false;
+
+    menuBtn.addEventListener("click", toggleMenu);
+    overlay.addEventListener("click", toggleMenu);
+
+    function toggleMenu() {
+      if (!showMenu) {
+        menuBtn.classList.add("clicked");
+        menu.classList.add("show");
+        menuUl.classList.add("show");
+        overlay.classList.add("show");
+
+        // * to hide scroll of the body when overflowing
+        // body.classList.add("overflow");
+
+        // * just a for each reference
+        // navItems.forEach(function(item) {
+        //   return item.classList.add("show");
+        // });
+
+        // Set Menu State
+        showMenu = true;
+      } else {
+        menuBtn.classList.remove("clicked");
+        menu.classList.remove("show");
+        menuUl.classList.remove("show");
+        overlay.classList.remove("show");
+
+        // * to hide scroll of the body when overflowing
+        // body.classList.remove("overflow");
+
+        // * just a for each reference
+        // navItems.forEach(function(item) {
+        //   return item.classList.remove("show");
+        // });
+
+        // Set Menu State
+        showMenu = false;
+      }
+    }
+  },
+  typingHeadline: function() {
+    // ****** ****** ****** ******
+    // START Typing Headlines
+    // ****** ****** ****** ******
+    var typed = new Typed("#typed", {
+      stringsElement: "#typed-strings",
+
+      typeSpeed: 100,
+      backSpeed: 80,
+      loop: true,
+      loopCount: 3,
+      startDelay: 500,
+      backDelay: 2000
+    });
+  },
+  // ****** ****** ****** ******
+  // START Isotope Settings
+  // ****** ****** ****** ******
+  isotope: function() {
+    $(".filters ul li").click(function() {
+      $(".filters ul li").removeClass("active");
+      $(this).addClass("active");
+
+      var data = $(this).attr("data-filter");
+      $grid.isotope({
+        filter: data
+      });
+    });
+
+    if (document.getElementById("portfolio")) {
+      var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: true,
+        masonry: {
+          columnWidth: ".all"
+        }
+      });
+    }
+  },
+  aos: function() {
+    // ****** ****** ****** ******
+    //  START AOS
+    // ****** ****** ****** ******
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: "ease-in-sine",
+      delay: 300,
+      disable: "mobile"
+    });
+  }
+
+  // END Objects
 };
 
 $("document").ready(function() {
   app.init();
-
-  $(".filters ul li").click(function() {
-    $(".filters ul li").removeClass("active");
-    $(this).addClass("active");
-
-    var data = $(this).attr("data-filter");
-    $grid.isotope({
-      filter: data
-    });
-  });
-
-  if (document.getElementById("portfolio")) {
-    var $grid = $(".grid").isotope({
-      itemSelector: ".all",
-      percentPosition: true,
-      masonry: {
-        columnWidth: ".all"
-      }
-    });
-  }
-});
-
-// Select DOM Items
-var menuBtn = document.querySelector(".menu-btn");
-var menu = document.querySelector(".menu");
-var menuUl = document.querySelector(".menu-ul");
-var navItems = document.querySelectorAll(".nav-item");
-var overlay = document.querySelector(".overlay");
-var body = document.querySelector("body");
-
-// Set Initial State Of Menu
-var showMenu = false;
-
-menuBtn.addEventListener("click", toggleMenu);
-overlay.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add("clicked");
-    menu.classList.add("show");
-    menuUl.classList.add("show");
-    overlay.classList.add("show");
-    // body.classList.add("overflow");
-    // navItems.forEach(function(item) {
-    //   return item.classList.add("show");
-    // });
-
-    // Set Menu State
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove("clicked");
-    menu.classList.remove("show");
-    menuUl.classList.remove("show");
-    overlay.classList.remove("show");
-    // body.classList.remove("overflow");
-    // navItems.forEach(function(item) {
-    //   return item.classList.remove("show");
-    // });
-
-    // Set Menu State
-    showMenu = false;
-  }
-}
-
-var typed = new Typed("#typed", {
-  stringsElement: "#typed-strings",
-
-  typeSpeed: 100,
-  backSpeed: 80,
-  loop: true,
-  loopCount: 3,
-  startDelay: 100,
-  backDelay: 2000
 });
--- dump.js
+++ dump.js
@@ -1,19 +1,67 @@
+//JQuery Module Pattern
+
+// An object literal
+var app = {
+  init: function() {
+    app.functionOne();
+  },
+  functionOne: function() {}
+};
+
+$("document").ready(function() {
+  app.init();
+
+  // ****** ****** ****** ******
+  // START Isotope Settings
+  // ****** ****** ****** ******
+  $(".filters ul li").click(function() {
+    $(".filters ul li").removeClass("active");
+    $(this).addClass("active");
+
+    var data = $(this).attr("data-filter");
+    $grid.isotope({
+      filter: data
+    });
+  });
+
+  if (document.getElementById("portfolio")) {
+    var $grid = $(".grid").isotope({
+      itemSelector: ".all",
+      percentPosition: true,
+      masonry: {
+        columnWidth: ".all"
+      }
+    });
+  }
+  // ****** ****** ****** ******
+  // END Isotope Settings
+  // ****** ****** ****** ******
+});
+
+// ****** ****** ****** ******
+// START Burger Menu
+// ****** ****** ****** ******
 // Select DOM Items
 var menuBtn = document.querySelector(".menu-btn");
 var menu = document.querySelector(".menu");
-var menuNav = document.querySelector(".menu-nav");
+var menuUl = document.querySelector(".menu-ul");
 var navItems = document.querySelectorAll(".nav-item");
+var overlay = document.querySelector(".overlay");
+var body = document.querySelector("body");
 
 // Set Initial State Of Menu
 var showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
-    menuBtn.classList.add("close");
+    menuBtn.classList.add("clicked");
     menu.classList.add("show");
-    menuNav.classList.add("show");
+    menuUl.classList.add("show");
+    overlay.classList.add("show");
+    // body.classList.add("overflow");
     // navItems.forEach(function(item) {
     //   return item.classList.add("show");
     // });
@@ -21,9 +69,11 @@ function toggleMenu() {
     // Set Menu State
     showMenu = true;
   } else {
-    menuBtn.classList.remove("close");
+    menuBtn.classList.remove("clicked");
     menu.classList.remove("show");
-    menuNav.classList.remove("show");
+    menuUl.classList.remove("show");
+    overlay.classList.remove("show");
+    // body.classList.remove("overflow");
     // navItems.forEach(function(item) {
     //   return item.classList.remove("show");
     // });
@@ -32,3 +82,32 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// ****** ****** ****** ******
+// END Burger Menu
+// ****** ****** ****** ******
+// ****** ****** ****** ******
+// START Typing Header
+// ****** ****** ****** ******
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+
+  typeSpeed: 100,
+  backSpeed: 80,
+  loop: true,
+  loopCount: 3,
+  startDelay: 500,
+  backDelay: 2000
+});
+// ****** ****** ****** ******
+//  END Typing Header
+// ****** ****** ****** ******
+
+AOS.init({
+  offset: 200,
+  duration: 600,
+  easing: "ease-in-sine",
+  delay: 300,
+  once: true,
+  disable: "mobile"
+});
